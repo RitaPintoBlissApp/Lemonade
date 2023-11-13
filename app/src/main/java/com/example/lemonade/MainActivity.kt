cc0906c0b186f32103e9bca411fafb8d3059f283
RitaPintoBissApp <rpinto@blissapplications.com>
2023-11-13 11:34:29 +0000

    Added a new way of doing it

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.example.lemonade
 
-import com.example.lemonade.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,8 +26,187 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                 AppLemonade()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppLemonade(modifier: Modifier = Modifier){
+    Column {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = "Lemonade",
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            colors = TopAppBarDefaults.smallTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        )
+        Surface (modifier = Modifier
+            .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background){
+            DiceWithButtonAndImage()
+        }
+
+    }
+
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    when (result){
+        1-> {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize() //Isso garante que os filhos dentro da coluna fiquem centralizados na tela do dispositivo em relação à largura.
+
+            ){
+                Image(
+                    painter = painterResource(R.drawable.lemon_tree),
+                    contentDescription = result.toString(),
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            result = 2
+                            squeezeCount = (2..4).random()
+                        })
+
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(text = stringResource(R.string.lemon_tree_text))
+
+            }
+        }
+        2 -> {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize() //Isso garante que os filhos dentro da coluna fiquem centralizados na tela do dispositivo em relação à largura.
+
+            ){
+               Image(
+                    painter = painterResource(R.drawable.lemon_squeeze),
+                    contentDescription = result.toString(),
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            squeezeCount--
+                            if (squeezeCount == 0) {
+                                result = 3
+                            }
+                        })
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(text = stringResource(R.string.lemon_text))
+
+            }
+        }
+        3 ->  {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize() //Isso garante que os filhos dentro da coluna fiquem centralizados na tela do dispositivo em relação à largura.
+
+            ){
+                Image(
+                    painter = painterResource(R.drawable.lemon_drink),
+                    contentDescription = result.toString(),
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            result = 4
+                        })
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(text = stringResource(R.string.lemon_glass_text))
+
+            }
+        }
+        4 -> {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize() //Isso garante que os filhos dentro da coluna fiquem centralizados na tela do dispositivo em relação à largura.
+
+            ){
+                Image(
+                    painter = painterResource(R.drawable.lemon_restart),
+                    contentDescription = result.toString(),
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clickable {
+                            result = 1
+                        })
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(text = stringResource(R.string.Empty_glass))
+
+            }
+        }
+    }
+
+}
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
+
+/*
+
+O código a baixo não está 100% certo pois não permite que se consiga programar quantas vezes seja necessário tocar numa imagem
+* package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -74,9 +252,6 @@ fun AppLemonade(modifier: Modifier = Modifier){
         )
         DiceWithButtonAndImage()
     }
-    //PageStructure(text = R.string.lemon_text,R.drawable.lemon_squeeze,1)
-   // PageStructure(text = R.string.lemon_juice_text, R.drawable.lemon_drink,1)
-    //PageStructure(text = R.string.lemon_glass_text, R.drawable.lemon_restart,1)
 
 }
 
@@ -120,3 +295,5 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 fun DiceRollerApp() {
     DiceWithButtonAndImage()
 }
+
+* */
